226c078f429a788e1f926f4c1b552bbc5817c361
ttully00 <tullyt@seattleu.edu>
2018-02-10 16:21:18 -0800

    changes2

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Rhymesaurus from '@/components/Rhymesaurus'
 import RhymeAdjective from '@/components/RhymeAdjective'
 
-
 Vue.use(Router)
 
 export default new Router({
